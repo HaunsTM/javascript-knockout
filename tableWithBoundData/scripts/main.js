31ec7bfa719539bff3361e4ee8e972f6c8ddc3a7
Hauns™ <hans.baldetorp@gmail.com>
2018-02-27 16:33:15 +0100

    Added many new features

--- tableWithBoundData/scripts/main.js
+++ tableWithBoundData/scripts/main.js
@@ -25,13 +25,25 @@ ko.dirtyFlag = function(root, isInitiallyDirty) {
     return result;
 };
 
- var ActivityCategory = function (categoryName, activities) {
-     var self = this;
-    this.categoryName = ko.observable(categoryName);
-    this.activities = ko.observableArray(activities);
-    
-    
+var Metadata =   function(lCIDDec, tableHeaderLabels) {
+    var self = this;
+    self.entityId = ko.observable(entityId);
 }
+
+var TableHeaderLabels = function (activityName, regarding, note, date, time, day, dependency, daysToAdd, timeTemplate, calculatedDayInFuture) {
+    var self = this;
+    self.activityName = ko.observable(activityName);
+    self.regarding = ko.observable(regarding);
+    self.note = ko.observable(note);
+    self.date = ko.observable(date);
+    self.time = ko.observable(time);
+    self.day = ko.observable(day);
+    self.dependency = ko.observable(dependency);
+    self.daysToAdd = ko.observable(daysToAdd);
+    self.timeTemplate = ko.observable(timeTemplate);
+    self.calculatedDayInFuture = ko.observable(calculatedDayInFuture);
+}
+
 
 var Activity = function (entityId, activityName, regarding, note, date, time, dependency, daysToAdd, timeTemplate, calculatedDayInFuture) {
     var self = this;
@@ -47,7 +59,7 @@ var Activity = function (entityId, activityName, regarding, note, date, time, de
         read: function () {
             var svLCIDDec = "1053";
             var timeCalculator = new namespace.TimeCalculator();
-            var dateObject = new Date(this.date());
+            var dateObject = new Date(self.date());
 
             return timeCalculator.getDayNameByLocaleCountryIdDecimal(dateObject, svLCIDDec);
         },
@@ -63,49 +75,17 @@ var Activity = function (entityId, activityName, regarding, note, date, time, de
     self.dirtyFlag = new ko.dirtyFlag(self);
 }
 
-var viewModel = function(items) {
-    
-    this.activityCategories = ko.observableArray([
-        new ActivityCategory("Order Dates", [
-                new Activity(1, "Header Venture booked", "", "Satsningen ska vara anmäld", "2017-12-04", "00:00", "Releaseday", "45", "00:00:00", "2017-12-04"),
-                new Activity(2, "Circulation Deadline Preliminary", "", "Ev preliminär upplaga", "", "", "Circulation Deadline Defined", "", "00:00:00", ""),
-                new Activity(3, "Circulation Deadline Definite", "", "Definitiv upplaga klar", "2018-01-19", "00:00", "Bindery Finished Sheet", "5", "08:30:00", "2018-01-19"),                
-                new Activity(4, "Print Order Deadline Preliminary", "", "Ev. preliminär tryckorder", "", "", "Print Order Deadline Defined", "", "00:00:00", ""),
-                new Activity(5, "Print Order Definte", "", "Definitiv tryckorder", "2018-01-22", "09:00", "Bindery Finished Sheet", "4", "09:00:00", "2018-01-22"),
-            ]
-        ),
-        new ActivityCategory("Repro", [
-                new Activity(6, "Sheet Repro", "Ark 1", "TV-sidor till ES senast 12.00", "2018-01-23", "00:00", "Printing", "", "00:00:00", "2018-01-23"),
-                new Activity(7, "Sheet Repro", "Ark 2", "Lämning nästa vecka + tablå", "2018-01-23", "00:00", "Printing", "", "00:00:00", "2018-01-23"),
-                new Activity(8, "Sheet Repro", "Ark 3 Kryss", "Slutlämning exkl sena sidor", "2018-01-23", "00:00", "Printing", "", "00:00:00", "2018-01-23"),
-            ]
-        ),
-        new ActivityCategory("Sheet Printing", [
-                new Activity(9, "Printing", "Ark 1", "Ark ska lämnas till tryck", "2018-01-23", "15:00", "Bindery Finished Sheet", "3", "15:00:00", "2018-01-23"),
-                new Activity(10, "Printing", "Ark 2", "Ark ska lämnas till tryck", "2018-01-23", "15:00", "Bindery Finished Sheet", "3", "15:00:00", "2018-01-23"),
-                new Activity(11, "Printing", "Ark 3 Kryss", "Ark ska lämnas till tryck", "2018-01-23", "15:00", "Bindery Finished Sheet", "3", "15:00:00", "2018-01-23"),
+var ActivityCategory = function (categoryName, activities) {
+    var self = this;
+    self.categoryName = ko.observable(categoryName);
+    self.activities = ko.observableArray(activities); 
+}
 
-            ]
-        ),
-        new ActivityCategory("Binding", [
-                new Activity(12, "Bindery Fished Sheet", "", "Ark till häftning", "2018-01-26", "00:00", "Edition Bindery", "0", "00:00:00", "2018-01-26"),
-                new Activity(13, "Edition Bindery", "", "Normal bindningsstart", "2018-01-26", "00:00", "Releaseday", "9", "00:00:00", "2018-01-26")
-            ]
-        )
-    ]);
-    
-};
-
-var ProductionScheduleModel = function(items) {
+var ProductionScheduleModel = function(metadata, items) {
     var self = this;
     
     self.activityCategories = ko.observableArray(items);
     
-    self.calculateGridBasedOnThisRow = function (activity) {        
-        var dataThatGridShouldBeRecalculatedOn = ko.toJSON(activity);
-        console.log( dataThatGridShouldBeRecalculatedOn);
-    };
-
     self.dirtyItems = ko.computed(function() {
         var tempDirtyItems = [];
         var activityCategories = self.activityCategories();
@@ -135,17 +115,29 @@ var ProductionScheduleModel = function(items) {
         return this.dirtyItems().length > 0;
     }, self);
 
+    self.calculateGridBasedOnThisRow = function (activity) {        
+        var dataThatGridShouldBeRecalculatedOn = ko.toJSON(activity);
+        //send dataThatGridShouldBeRecalculatedOn to server
+        //wait for response, when it arrives
+        console.log( dataThatGridShouldBeRecalculatedOn);
+    };
+
     self.clickedOnSave = function (model, event) {
         var dataToSave = ko.toJSON(self.dirtyItems);
         console.log('Saving data: '+ dataToSave );
+        self.activityCategories(viewModel1);
     }
 
-    this.clickedOnSaveAndExit = function (model, event) {        
+    self.clickedOnSaveAndExit = function (model, event) {        
         console.log('Closing');
     };
 }
 
-var viewModel = new ProductionScheduleModel([
+var viewModel = new ProductionScheduleModel({
+    lCIDDec : "1053"
+},
+    
+    [
     new ActivityCategory("Order Dates", [
             new Activity(1, "Header Venture booked", "", "Satsningen ska vara anmäld", "2017-12-04", "00:00", "Releaseday", "45", "00:00:00", "2017-12-04"),
             new Activity(2, "Circulation Deadline Preliminary", "", "Ev preliminär upplaga", "", "", "Circulation Deadline Defined", "", "00:00:00", ""),
@@ -173,5 +165,22 @@ var viewModel = new ProductionScheduleModel([
         ]
     )
 ]);
+
+var viewModel1 = [
+    new ActivityCategory("Order Dates", [
+            new Activity(1, "1 Header Venture booked", "", "Satsningen ska vara anmäld", "2017-12-04", "00:00", "Releaseday", "45", "00:00:00", "2017-12-04"),
+            new Activity(2, "1 Circulation Deadline Preliminary", "", "Ev preliminär upplaga", "", "", "Circulation Deadline Defined", "", "00:00:00", ""),
+            new Activity(3, "1 Circulation Deadline Definite", "", "Definitiv upplaga klar", "2018-01-19", "00:00", "Bindery Finished Sheet", "5", "08:30:00", "2018-01-19"),                
+            new Activity(4, "1 Print Order Deadline Preliminary", "", "Ev. preliminär tryckorder", "", "", "Print Order Deadline Defined", "", "00:00:00", ""),
+            new Activity(5, "1 Print Order Definte", "", "Definitiv tryckorder", "2018-01-22", "09:00", "Bindery Finished Sheet", "4", "09:00:00", "2018-01-22"),
+        ]
+    ),
+    new ActivityCategory("Repro", [
+            new Activity(6, "1 Sheet Repro", "Ark 1", "TV-sidor till ES senast 12.00", "2018-01-23", "00:00", "Printing", "", "00:00:00", "2018-01-23"),
+            new Activity(7, "1 Sheet Repro", "Ark 2", "Lämning nästa vecka + tablå", "2018-01-23", "00:00", "Printing", "", "00:00:00", "2018-01-23"),
+            new Activity(8, "1 Sheet Repro", "Ark 3 Kryss", "Slutlämning exkl sena sidor", "2018-01-23", "00:00", "Printing", "", "00:00:00", "2018-01-23"),
+        ]
+    )
+];
 
 ko.applyBindings(viewModel);
